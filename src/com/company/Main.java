57d27356f78f1dc86ed75e361ff3350bd072c83f
LEv145 <40071054+LEv145@users.noreply.github.com>
2020-06-10 23:20:20 +0200

    Create Main.java

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.lang.String;
+
+
+public class Main {
+	public static void main(String[] args) {
+		
+
+		Student lev = new Student(20,200,"Lev",1);
+		lev.tell();
+
+
+
+		
+	}
+}
