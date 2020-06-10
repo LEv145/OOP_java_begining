f77d1afbbccd311ff305a3b77783eb57246075c6
LEv145 <40071054+LEv145@users.noreply.github.com>
2020-06-10 23:20:49 +0200

    Create Person.java

---
+++ src/com/company/Person.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.lang.String;
+
+public class Person {
+
+	//Дефолт
+	public int age = 20;
+	public int height = 200;
+	public String name = "No name";
+
+
+
+
+	public Person(int a, int h, String n){
+		age = age;
+		height = h;
+		name = n;
+
+	}
+
+
+
+
+}
